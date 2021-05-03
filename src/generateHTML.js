2bd8d37bc92b2c903a1b8b65683cdbafa5aa9a51
Tomeeka Lilly <tomeeka.lilly1@gmail.com>
2021-05-02 20:20:06 -0400

    Update the style.css and the generateHTML

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,48 +1,58 @@
 function cardManager(data) {
-    return `<div class="employeeCard ml-3 border border-primary">
-    <div class="card-header">
-        <h2 class="card-title">${data.name}</h2>
-        <h3 class="card-title"><span class="material-icons">assignment</span>Manager</h3>
+    return `<div class="card" style="max-width: 18rem;">
+    <div class="employeeCard">
+        <div class="card-header">
+            <h2>${data.name}</h2>
+            <h3 class="card-title"><span class="material-icons">assignment</span>Manager</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item"><strong>ID:</strong> ${data.id}</li>
+                <li class="list-group-item"><strong>Email: </strong> ${data.officeNumber}</li>
+                <li class="list-group-item"><strong>Office Number:</strong> 1</li>
+            </ul>
+        </div>                
     </div>
-    <div class="card-body">
-        <ul class="list-group list-group-flush ">
-            <li class="list-group-item"><strong>ID:</strong>${data.id}</li>
-            <li class="list-group-item"><strong>Email: </strong>${data.email}</li>
-            <li class="list-group-item"><strong>Office Number:</strong>${data.officeNumber}</li>
-        </ul>
-    </div>
-</div>`;
-}
+</div>`   
+};
+
+
 function cardEngineer(data) {
-    return `<div class="employeeCard ml-3 border border-primary ">
-    <div class="card-header">
-        <h2 class="card-title">${data.name}</h2>
-        <h3 class="card-title"><span class="material-icons">engineering</span>Engineer</h3>
-    </div>
-    <div class="card-body">
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item"><strong>ID:</strong>${data.id}</li>
-            <li class="list-group-item"><strong>Email: </strong>${data.email}</li>
-            <li class="list-group-item"><strong>GitHub:</strong>${data.gitHub}</li>
-        </ul>
+    return `<div class="card" style="max-width: 18rem;">
+    <div class="employeeCard">
+        <div class="card-header">
+            <h2>${data.name}</h2>
+            <h3 class="card-title"><span class="material-icons">assignment</span>Engineer</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item"><strong>ID:</strong> ${data.id}</li>
+                <li class="list-group-item"><strong>Email: </strong> ${data.email}</li>
+                <li class="list-group-item"><strong>GitHub:</strong> ${data.gitHub}</li>
+            </ul>
+        </div>                
     </div>
 </div>`
-}
+};
+
+
 function cardIntern(data) {
-    return `<div class="employeeCard ml-3 border border-primary ">
-    <div class="card-header">
-        <h2 class="card-title">Tiger Team Profile</h2>
-        <h3 class="card-title"><span class="material-icons">school</span>Intern</h3>
+    return `<div class="card" style="max-width: 18rem;">
+    <div class="employeeCard">
+        <div class="card-header">
+            <h2>${data.name}</h2>
+            <h3 class="card-title"><span class="material-icons">assignment</span>Intern</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item"><strong>ID:</strong> ${data.id}</li>
+                <li class="list-group-item"><strong>Email: </strong> ${data.email}</li>
+                <li class="list-group-item"><strong>School: </strong> ${data.school}</li>
+            </ul>
+        </div>                
     </div>
-    <div class="card-body">
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item"><strong>ID:</strong>${data.id}</li>
-            <li class="list-group-item"><strong>Email: </strong>${data.email}</li>
-            <li class="list-group-item"><strong>School:</strong>${data.school}</li>
-        </ul>
-    </div>
-</div>`
-}
+</div>`    
+};
 
 function generateHTML(teamMembers) {
     let employeeCard = "";
@@ -55,37 +65,40 @@ function generateHTML(teamMembers) {
             employeeCard += cardIntern(member);
         };
     }) 
+
      return `<!DOCTYPE html>
      <html lang="en">
      <head>
          <meta charset="UTF-8">
          <meta http-equiv="X-UA-Compatible" content="IE=edge">
          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+         <link rel="stylesheet" href="style.css">
          <title>Team Profile</title>
-         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-         <link href="./fontawesome-free-5.15.3-web/css/all.css" rel="stylesheet">
-         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-         <link rel="preconnect" href="https://fonts.gstatic.com">
-         <link href="https://fonts.googleapis.com/css2?family=Karantina:wght@300&display=swap" rel="stylesheet">
-         <link href="./output/style.css" rel="stylesheet">
      </head>
      
      <body>
          <header>
-             <h1 class="display-4 justify-content-center">Tiger Team</h1>
+             <div class="jumbotron jumbotron-fluid">
+                 <div class="container">
+                     <h1 class="display-4 justify-content-center">Tiger Team</h1>
+                 </div>
+             </div>
          </header>
-         <br>
 
          <section>
-             <div id="roles" class="container">
-             ${employeeCard}            
-             </div>
-         </section>
-         
-         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
-         
-     </body>
-     </html>`
-}
+            <div class="card-deck" id="roles">
+
+            ${employeeCard}  
+            
+            
+            </div>    
+        </section>
+            
+    </body>
+    </html>`
+};
+
 module.exports = generateHTML;
